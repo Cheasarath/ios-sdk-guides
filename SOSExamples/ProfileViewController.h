bcd5171f792489a3e5afde033760f07e00d8a961
Jeremy Wright <jeremywright@goinstant.com>
2014-09-16 17:40:51 -0300

    Add view controller

---
+++ SOSExamples/ProfileViewController.h
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.h
+//  SOSExamples
+//
+//  Created by Jeremy Wright on 9/16/14.
+//  Copyright (c) 2014 goinstant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIView *view;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *profileView;
+@end
